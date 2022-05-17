75e21978a0c3bc0a339fc5b21002918299b8739b
Nis-chal <nischalchan@gmail.com>
2022-05-17 21:57:17 +0545

    login design was created

---
+++ lib/screens/loginScreen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:socialmedia/components/roundedbtn.dart';
+import '../constants.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool showSpinner = false;
+  late String email;
+  late String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: ModalProgressHUD(
+            inAsyncCall: showSpinner,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SizedBox(
+                  height: 70,
+                  child: Hero(
+                    tag: 'logo',
+                    child: Flexible(
+                      // child: Hero(tag: 'logo', child: ),
+                      child: Image.asset('assets/images/camera.png'),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: Flexible(
+                    child: Image.asset('assets/images/Winkle.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                // Image.asset('assets/images/download.jpg'),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.left,
+                  onChanged: (value) {
+                    email = value;
+                    //Do something with the user input.
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your email'),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  obscureText: true,
+
+                  // textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                    //Do something with the user input.
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password'),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(
+                  title: 'Log In',
+                  colour: Color(0xFF363636),
+                  onPressed: () {},
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Dont't have account ",
+                      textAlign: TextAlign.end,
+                    ),
+                    SizedBox(
+                      width: 4.0,
+                    ),
+                    GestureDetector(
+                      child: Text(
+                        ' Register',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      // onTap: () {
+                      //   Navigator.pushNamed(context, RegisterScreen.id);
+                      // },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
