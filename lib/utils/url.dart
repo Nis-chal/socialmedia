95f196c86be76abf4a92531ba8b6830fe5ee7926
Nis-chal <nischalchan@gmail.com>
2022-06-09 15:30:37 +0545

    dio setup was done

---
+++ lib/utils/url.dart
@@ -0,0 +1,8 @@
+const baseUrl = "http://10.0.2.2:5000/api/v1/";
+
+
+const productUrl = "";
+const loginUrl = "user/login";
+const registerUrl = "user";
+const getProductsUrl = "products";
+String? token;
