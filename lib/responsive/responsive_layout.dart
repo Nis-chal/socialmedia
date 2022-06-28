dedbaf9e3ae81161d0386d807759c27da85350e4
Nis-chal <nischalchan@gmail.com>
2022-06-28 07:10:57 +0545

    layout was separeted for wearOs

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileBody;
+  final Widget desktopBody;
+  
+
+  ResponsiveLayout({required this.mobileBody,required this.desktopBody});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
